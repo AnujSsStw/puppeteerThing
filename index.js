dc112c38df17cae0a93d9b032293f61f88438568
Anuj Singh <anujZzztw@gmail.com>
2022-03-22 15:22:25 +0530

    cleared up some things

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ async function start() {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.setDefaultNavigationTimeout(0);
-    await page.goto('https://www.systemrequirementslab.com/game-lists/third-person-shooters/?id=1097') //replace the link from which you wanna extract the text and img
-    await page.screenshot({ path: 'game.png' })
+    await page.goto('') //replace the link from which you wanna extract the text and img
+    await page.screenshot({ path: '/ScreenShots/' })
 
     const headline = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#main-table > tbody > tr > td:nth-child(2) > div > div:nth-child(2) > main > div > div:nth-child(1) > div > div:nth-child(1) > div > h1')
+        return Array.from(document.querySelectorAll('') // selector which you wanna scrape the text
         ).map(x => x.textContent)
     })
 
-    await fs.writeFile('headlinegame.txt', headline.join("\r\n"))
+    await fs.writeFile('/Headline/', headline.join("\r\n"))
 
     await browser.close();
 
